e9a383d8d3e102f72a1154a645f7fc2e7adad69b
Nick Tobey <nick@dolthub.com>
2023-05-16 12:13:55 -0700

    Fix compiliation errors in mysql_connector_test.rs

--- integration-tests/mysql-client-tests/rust/src/mysql_connector_test.rs
+++ integration-tests/mysql-client-tests/rust/src/mysql_connector_test.rs
@@ -1,46 +1,29 @@
-//use mysql::*;
-//use mysql::prelude::*;
+extern crate mysql;
+
+use mysql::*;
+use mysql::prelude::*;
 use std::env;
 use std::process::exit;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let user = &args[1];
-    let port = &args[2];
-    let db = &args[3];
-
-    let url = format!("mysql://{}@127.0.0.1:{}/{}", user, port, db);
-    //let pool = Pool::new(url).unwrap();
-    //let mut conn = pool.get_conn().unwrap();
-    println!("USER: {}", user);
-    println!("PORT: {}", port);
-    println!("DB: {}", db);
-    println!("URL: {}", url);
-
-    exit(0)
-}
-
-/*
 use std::collections::HashMap;
 
 
-// queries
-const QUERY_RESPONSE: HashMap<&str, i32> = HashMap::from([
-("create table test (pk int, `value` int, primary key(pk))", 0),
-("describe test", 3),
-("insert into test (pk, `value`) values (0,0)", 1),
-("select * from test", 1),
-("call dolt_add('-A');", 1),
-("call dolt_commit('-m', 'my commit')", 1),
-("call dolt_checkout('-b', 'mybranch')", 1),
-("insert into test (pk, `value`) values (1,1)", 1),
-("call dolt_commit('-a', '-m', 'my commit2')", 1),
-("call dolt_checkout('main')", 1),
-("call dolt_merge('mybranch')", 1),
-("select COUNT(*) FROM dolt_log", 1)
-]);
-
 fn main() {
+    // queries
+    let queries: HashMap<&str, i32> = HashMap::from([
+        ("create table test (pk int, `value` int, primary key(pk))", 0),
+        ("describe test", 3),
+        ("insert into test (pk, `value`) values (0,0)", 1),
+        ("select * from test", 1),
+        ("call dolt_add('-A');", 1),
+        ("call dolt_commit('-m', 'my commit')", 1),
+        ("call dolt_checkout('-b', 'mybranch')", 1),
+        ("insert into test (pk, `value`) values (1,1)", 1),
+        ("call dolt_commit('-a', '-m', 'my commit2')", 1),
+        ("call dolt_checkout('main')", 1),
+        ("call dolt_merge('mybranch')", 1),
+        ("select COUNT(*) FROM dolt_log", 1)
+    ]);
     // get CL args
     let args: Vec<String> = env::args().collect();
     let user = &args[1];
@@ -48,27 +31,33 @@ fn main() {
     let db = &args[3];
 
     // open connection
-    let client = HashMap::from([
-        ("user", user),
-        ("host", "127.0.0.1"),
-        ("port", port),
-        ("db_name", db)
+    let client: HashMap<String, String> = HashMap::from([
+        (String::from("user"), String::from(user)),
+        (String::from("host"), String::from("127.0.0.1")),
+        (String::from("port"), String::from(port)),
+        (String::from("db_name"), String::from(db))
     ]);
-    let mut builder = OptsBuilder::new().from_hash_map(client);
+    let builder_result = OptsBuilder::new().from_hash_map(&client);
     //let url = "mysql://" + user + "@localhost:127.0.0.1/" + db;
     //let pool = Pool::new(url).unwrap();
-    let mut conn = builder.get_conn().unwrap();
+    let conn = builder_result.expect("Error: bad connection");
+    // let mut conn = builder.get_conn().unwrap();
+
+    let pool = Pool::new(conn).unwrap();
+    let mut conn = pool.get_conn().unwrap();
 
     // for query in query_response...execute query
-    for (query, exp_result) in QUERY_RESPONSE.iter() {
-        let result = conn.query_map(query);
-        if result != exp_result {
+    for (query, exp_result) in queries.iter() {
+        let result = conn.query(query);
+        let response : Vec<Row> = result.expect("Error: bad response");
+        println!("{:?}", response);
+        //if response.len() != exp_result {
             println!("QUERY: {}", query);
             println!("EXPECTED: {}", exp_result);
-            println!("RESULT: {}", result);
+            println!("RESULT: {:?}", response);
             exit(1)
-        }
+        //}
     }
     exit(0)
 
-}*/
+}
